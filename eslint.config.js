39a0de453c147e1bd2c3b96bc96a3cc15bff5439
Tanish Baidya <tanish.baidya@gmail.com>
2026-04-20 22:08:38 +0100

    chore: add nextjs project

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,9 @@
 // eslint.config.js - ESLint v9+ flat config
+// Note: apps/ folder is excluded - each app manages its own linting
 export default [
+  {
+    ignores: ['apps/**']
+  },
   {
     rules: {
       // Add custom rules here
